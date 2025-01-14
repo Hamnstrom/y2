e2734c11ee52448b82bb9122840e340fcf0c3f8b
Hamnstrom <150655352+Hamnstrom@users.noreply.github.com>
2025-01-14 09:53:23 +0200

    14.1.2024

---
+++ PHPövn1/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+
+        <?php 
+            
+            $myschool = "Prakticum";
+            $intEx = 2;
+            $floatEx = 2.5;
+            echo "$myschool\n";
+            echo "$intEx * $floatEx = " . $intEx * $floatEx . "\n";
+            
+            $link = "https://prakticum.fi/";
+            echo "<a href='$link'>$link</a>\n";
+            
+        ?>
+
+        
+        <p>test</p>
+    </body>
+</html>
